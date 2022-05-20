7c8f2d7f202c901fa6d8059e71e9706a6948d718
khiem4 <khiemcan@gmail.com>
2022-05-20 22:49:29 +0700

    Finish rock paper scissor project

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
-'user stict';
+const rPs = ['rock', 'paper', 'scissor'];
+let userScore = 0;
+let computerScore = 0;
 
-///Step 1:
-///Start with a function called computerPlay can randomly return R-P-S
-///Use the function to make the computer's play
-///use console to make sure it returning the expected output
-
-const rPs = ['Rock', 'Paper', 'Scissor'];
 
 function computerPlay() {
-    return console.log(rPs[Math.floor(Math.random() * rPs.length)]);
+    return rPs[Math.floor(Math.random() * rPs.length)];
 }
-computerPlay();
+
+function playerPlay() {
+    let playerSelection = prompt('Input here').toLowerCase();
+    return playerSelection;
+}
+
+
+
+function playRound() {
+    let computerSelection = computerPlay();
+    let playerSelection = playerPlay();
+    if (playerSelection === computerSelection) {
+        return 'Draw';
+    } else if (
+        (playerSelection == 'rock' && computerSelection == 'scissor') ||
+        (playerSelection == 'paper' && computerSelection == 'rock') ||
+        (playerSelection == 'scissor' && computerSelection == 'paper')) {
+        userScore++;
+        return 'You win';
+    } else {
+        computerScore++;
+        return 'You lose';
+    }
+}
+
+
+function game() {
+    for (let i = 1; i <= 5; i++) {
+        console.log(playRound());
+        console.log((`Player Score: ${userScore} Computer Score: ${computerScore}`));
+    }
+    if (userScore > computerScore) return 'Player Win';
+    else if (userScore == computerScore) return 'Tie'
+    else { return 'Computer Win' }
+
+}
+
+console.log(game());
+
+
 
 
 
