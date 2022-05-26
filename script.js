dffce21cd4f545af54ce2aad08af186a7c0b37cf
khiem4 <khiemcan@gmail.com>
2022-05-27 02:33:32 +0700

    Counting and annouce score

--- script.js
+++ script.js
@@ -1,40 +1,50 @@
 const rPs = ['rock', 'paper', 'scissor'];
-let userScore = 0;
-let computerScore = 0;
+let userScore = 1;
+let computerScore = 1;
 let playerChoice;
+
+const result = document.querySelector('.result');
+const comScore = document.querySelector('#score');
+const uScore = document.querySelector('#score');
+
 function computerPlay() {
     return rPs[Math.floor(Math.random() * rPs.length)];
 }
 
 
-
-
 function playRound(playerSelection, computer = computerPlay()) {
-
     if (playerSelection == computer) {
+        uScore.textContent = 'Draw';
         return 'Draw';
     } else if (
         (playerSelection == 'rock' && computer == 'scissor') ||
         (playerSelection == 'paper' && computer == 'rock') ||
         (playerSelection == 'scissor' && computer == 'paper')) {
-        userScore++;
+        uScore.textContent = 'Player win ' + userScore++ + ' time';
         return 'You win';
     } else {
-        computerScore++;
+        comScore.textContent = 'Computer win ' + computerScore++ + ' time';
         return 'You lose';
     }
 }
 
-console.log(playRound());
+
+function checkWinner() {
+    if (userScore == 5) alert('Player win');
+    if (computerScore == 5) alert('Computer win')
+}
+console.log(checkWinner());
+
+
 
 const buttons = document.querySelectorAll('button');
-
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
         playerChoice = button.getAttribute('class');
         console.log(playRound(playerChoice));
     });
-})
+});
+
 
 
 
